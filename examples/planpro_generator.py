883c15a296fcb2e8bc61dcd44064d081cd0a75c2
Benedikt Schenkel <benedikt.schenkel@student.hpi.de>
2023-01-10 13:33:45 +0100

    remove names from example

--- examples/planpro_generator.py
+++ examples/planpro_generator.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     _cli.run()
     generator = Generator()
     generator.generate(_cli.topology,
-        author_name="Arne Boockmeyer",
-        organisation="HPI.OSM", filename="Test")
+        author_name="Your Name",
+        organisation="Your Organization", filename="Export")
     print("Generation completed")
     print("Generator terminates.")
